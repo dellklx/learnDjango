3ab10e472dcae2a264105e54a21b0585577d1fed
dellklx <dellklx@126.com>
2017-05-14 21:35:38 +0800

    change title

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True       #add Ture icon
+    was_published_recently.short_description = 'Published recently?'
 
 @python_2_unicode_compatible
 class Choice(models.Model):
